296e49029caf531fbd8d6cfd66a1fb8c06c96dcf
Katherine G. Pe <katherinepe@me.com>
2016-07-28 20:32:54 +0800

    Unit tests for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   validates :user_id, presence: true
   validates :name, presence: true, uniqueness: true
+  validates :subdomain, presence: true, uniqueness: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Company do
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:subdomain) }
 end
